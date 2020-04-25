3ca896f0f24c85485fe1c02770abb4a2db71c3c2
102101219 <62124876+102101219@users.noreply.github.com>
2020-04-25 23:13:21 +1000

    Made It Go BURRRRR
    Replaced the shitty java with less shitty c#

---
+++ DiceRoller/DiceRoller/DiceRoller/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace DiceRoller
+{
+    class MainClass
+    {
+
+  
+
+        public static void Main(string[] args)
+        {
+
+
+            Random rnd = new Random();
+            string option;
+            bool run = true;
+            while (run) 
+            {
+                int result = rnd.Next(1,7);
+                Console.WriteLine("Dice face value:" + result);
+               Draw(result);
+                Console.Write("Roll again? (type no to quit)");
+                option = Console.ReadLine();
+                run = !option.Contains("n");
+                if (!run) 
+                { 
+                Console.WriteLine("Bye!");
+                 }
+
+            }
+
+
+            void Draw(int result)
+            {
+                switch (result)
+                {
+                    case 1:
+                        Console.WriteLine("---------");
+                        Console.WriteLine("-\t-");
+                        Console.WriteLine("-   O   -");
+                        Console.WriteLine("-\t-");
+                        Console.WriteLine("---------");
+                        break;
+                    case 2:
+                        Console.WriteLine("---------");
+                        Console.WriteLine("-O      -");
+                        Console.WriteLine("-\t-");
+                        Console.WriteLine("-      O-");
+                        Console.WriteLine("---------");
+                        break;
+                    case 3:
+                        Console.WriteLine("---------");
+                        Console.WriteLine("-O      -");
+                        Console.WriteLine("-   O   -");
+                        Console.WriteLine("-      O-");
+                        Console.WriteLine("---------");
+
+                        break;
+                    case 4:
+                        Console.WriteLine("---------");
+                        Console.WriteLine("-O     O-");
+                        Console.WriteLine("-       -");
+                        Console.WriteLine("-O     O-");
+                        Console.WriteLine("---------");
+                        break;
+                    case 5:
+                        Console.WriteLine("---------");
+                        Console.WriteLine("-O     O-");
+                        Console.WriteLine("-   O   -");
+                        Console.WriteLine("-O     O-");
+                        Console.WriteLine("---------");
+
+                        break;
+                    case 6:
+                        Console.WriteLine("---------");
+                        Console.WriteLine("-O     O-");
+                        Console.WriteLine("-O     O-");
+                        Console.WriteLine("-O     O-");
+                        Console.WriteLine("---------");
+
+                        break;
+                }
+            }
+
+        }
+    }
+}
